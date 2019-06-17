763b5777a99b6f78bf6285f2ce23d5453ad93d9f
Kiwamu Nakasone <nk@example.com>
2019-06-17 22:56:02 +0900

    add basical explaination

--- opencv/read-show.py
+++ opencv/read-show.py
@@ -2,7 +2,25 @@
 # -*- coding:utf-8 -*-
 import cv2
 
+#read image
 img = cv2.imread('/home/puppy/pic/pillowBasic001.jpg', cv2.IMREAD_COLOR)
+#display image
 cv2.imshow("image",img)
 cv2.waitKey(0)
+
+#overwriting image to existed window
+img = cv2.imread('/home/puppy/pic/UriHamushi.jpg', cv2.IMREAD_COLOR)
+cv2.namedWindow("image",cv2.WINDOW_NORMAL)
+cv2.imshow("image",img)
+cv2.waitKey(0)
+
+#open many windows
+cv2.imshow("image2",img)
+cv2.imshow("image3",img)
+#wait 5 seconds and close one windo
+cv2.waitKey(5000)
+cv2.destroyWindow("image3")
+cv2.waitKey(0)
+
+#close all windows
 cv2.destroyAllWindows()
