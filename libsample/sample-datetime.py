6419417e6d6e8723eb1a12fdd01ab223a2443be6
Kiwamu Nakasone <nk@example.com>
2019-06-20 23:50:42 +0900

    add debug hour,minutes,seconds

--- libsample/sample-datetime.py
+++ libsample/sample-datetime.py
@@ -1,5 +1,4 @@
 
-
 from datetime import datetime,timedelta,timezone
 
 jst = timezone(timedelta(hours=9))
@@ -7,7 +6,9 @@ today = datetime.now(jst)
 print(today)
 print(today.year)
 print(today.month)
-print(today.day)
+print(today.hour)
+print(today.minute)
+print(today.second)
 
 day = datetime.strptime("2030/01/10 06:02:19", "%Y/%m/%d %H:%M:%S")
 print(day)
