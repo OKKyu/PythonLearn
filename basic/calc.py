090b29cf658d64fff1c4249967b7fa3bd9d160b4
Kiwamu Nakasone <nk@example.com>
2019-07-23 22:43:52 +0900

    軽微な修正

--- basic/calc.py
+++ basic/calc.py
@@ -1,36 +1,32 @@
 #! python3
 # -*- coding: UTF-8 -*-
-# �l�����Z�̊�{
+# 四則演算の基本
 import sys
 
-# �l�����Z��Java��JavaScript�Ɠ����B
-#����Z������ƕԋp�l�͏����_�ƂȂ�B
+# 四則演算はJavaやJavaScriptと同じ。
+#割り算をすると返却値は小数点となる。
 print("1+2 " + str(1+2))
 print("3-1 " + str(3-1))
 print("6*5 " + str(6*5))
 print("6/2 " + str(6/2))
 print("6%5 " + str(6%5))
 
-#python�ł͏����_�ȉ���؂�̂ď��Z������ꍇ��//���g���B
+#pythonでは小数点以下を切り捨て除算をする場合は//を使う。
 print("6//2 " + str(6//2))
 
-#python�ł͗ݏ�v�Z��**���g���Čv�Z�ł���B�ȉ���6��2��
+#pythonでは累乗計算を**を使って計算できる。以下は6の2乗
 print("6**2 " + str(6**2))
 
-#���� = �͕ϐ��ւ̒l����B
-#�܂��A�ϐ��͌^��錾���Ȃ��ł悢�B�����Ō^�����肳��Ă���B
+#等号 = は変数への値代入。
+#また、変数は型を宣言しないでよい。自動で型が決定されている。
 width = 55
 height = 20
 print("width = 55")
 print("height = 20")
 print("width * height " + str(width * height))
 
-#�l�������Ȃ��ϐ��̐錾�͕s�\�B
-#�ȉ��R�����g�A�E�g���O����"name not defined"��O����������B
-#n
+#値を代入しない変数の宣言は不可能。
+#以下コメントアウトを外すと"name not defined"例外が発生する。
 
-#�Θb���[�h����
-#�Ō�ɕ\�����ꂽ���ʂ͕ϐ� _ �ɑ�������B
-
-
-
+#対話モード限定
+#最後に表示された結果は変数 _ に代入される。
--- libsample/sample-re.py
+++ libsample/sample-re.py
@@ -7,4 +7,7 @@ import re
 #first arguments is pattern. 2nd is target string.
 #if matches, return match object about that first position.
 m = re.search('def', 'abcdef')
-m.group(0)
+print(m.group(0))
+
+#re.sub（正規表現, “置換する文字列”, 置換対象の文字列）
+print(re.sub("a","b","abds"))
