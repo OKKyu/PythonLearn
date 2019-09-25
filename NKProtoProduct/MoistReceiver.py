69087ad6e826fad7f46eb9d55aaff98349439c83
Kiwamu Nakasone <nk@example.com>
2019-09-25 23:16:18 +0900

    sqliteに湿り気データを記録する処理を追加。1:接続およびテーブル生成処理の追加  2:情報保存処理の追加  3:接続失敗時にもflaskは起動させるが、保存処理は実行しない

--- NKProtoProduct/MoistReceiver.py
+++ NKProtoProduct/MoistReceiver.py
@@ -4,20 +4,56 @@
 #    flask run
 import socket
 from pathlib import Path
+import datetime
 from flask import Flask, request
+import sqlite3 as sqlite
+
+#initialize
 app = Flask(__name__)
+conn = __connectDb__()    
 
 @app.route('/moistinfo', methods=['POST'])
 def moistinfo():
     sendedIP = request.remote_addr
-    #moistpoint = request.form["VCC"]
-    #app.logger.debug("value:" + str(moistpoint))
     app.logger.debug("data send from:" + str(sendedIP))
     app.logger.debug("data point:" + str(request.json["VCC"]))
     app.logger.debug("receive ok to " + socket.gethostname())
     
+    __saveMoistInfo(sendedIP,request.json["VCC"])
+    
     return "receive ok to " + socket.gethostname()
 
+def __connectDb__():
+    conn = sqlite3.connect('MoistInfo.db')
+    c = conn.cursor()
+    try:
+        c.execute('select * from moist')
+    except OperationalError as operr:
+        createTable = 'CREATE TABLE moist (id INTEGER PRIMARY KEY AUTOINCREMENT, t timestamp, ipaddr TEXT, moistpoint INTEGER )'
+        c.execute(createTable)
+    except Exception:
+        conn.rollback()
+        conn.close()
+        conn = None
+    finally:
+        pass
+        
+    return conn
+
+def __saveMoistInfo(ipaddr, moistpt):
+    c = conn.cursor()
+    try:
+        c.execute('insert into moist (t, ipaddr, moistpoint) values (?,?,?)', (datetime.now(), ipaddr, moistpt))
+    except Exception:
+        conn.rollback()
+        print('failed insert information. rollback.')
+    finally:
+        conn.commit()
+        
 if __name__ == '__main__':
-    #app.run()
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    if conn is not None:
+        #app.run()
+        app.run(host='0.0.0.0', debug=True, port=5001)
+    else:
+        print('connection is failed. stop run MoistReceiver.py')
+
