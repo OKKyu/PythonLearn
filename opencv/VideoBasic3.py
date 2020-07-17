884dbef24b0f73d7d701c65eb8d6ab40c48ecc6f
Kiwamu Nakasone <kyukyuinskt@gmail.com>
2020-07-17 10:45:53 +0900

    複数画像の差分でノイズ除去ができないか試行錯誤したが無理だった。

--- opencv/VideoBasic3.py
+++ opencv/VideoBasic3.py
@@ -7,38 +7,58 @@ import cv2
 import sys,traceback
 import numpy as np
 
+def adjustImShow(wname,img,height,width):
+    cv2.namedWindow(wname, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(wname,height,width)
+    cv2.imshow(wname,img)
+
 capture = None
 try:
     capture = cv2.VideoCapture(0)
     
+    imgList = []
     while True:
         ret, img = capture.read()
         
         #display simple
-        cv2.imshow("simple", img)
+        adjustImShow("simple", img,800,800)
 
         #gray scale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("cvtColor", gray)
-
-        #hist
-        cv2.imshow("equalizeHist", cv2.equalizeHist(gray))
-
-        #canny
-        canny = cv2.Canny(img,40.0,200.0)
-        cv2.imshow("canny", canny)
+        adjustImShow("gray", gray,800,800)
         
-        #laplacian and erode
-        eroded = cv2.erode(img, np.ones((3,3), np.uint8))
-        lap = cv2.Laplacian(eroded, -1)
-        cv2.imshow("laplacian", lap)
+        #canny
+        #canny = cv2.Canny(img,40.0,200.0)
+        canny = cv2.Laplacian(img, -1)
+        adjustImShow("canny", canny,800,800)
+        
+        #noize reduction
+        if len(imgList) > 10:
+            imgList.pop(0)
+            reduced = np.average(imgList, axis=(0))
+            reduced = np.array(reduced, dtype=np.uint8)
+            #reduced = cv2.Canny(reduced, 40.0, 200.0)
+            reduced = cv2.Laplacian(reduced, -1)
+            adjustImShow("reduced", reduced,800,800)
+            
+        imgList.append(img)
+            
+        #ret,dst = cv2.threshold(lap,15,255,cv2.THRESH_BINARY)
+        #contours = cv2.findContours(gaus, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+        #for contour in contours:
+        #    if cv2.contourArea(contour) > int(500):
+        #        x,y,w,h = cv2.boundingRect(contour)
+        #        cv2.rectangle(dst,(x,y), (x+w,y+h), (0,255,0),5)        
+        
+        #adjustImShow("dst", dst,800,800)
         
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
             break
     
 except Exception as ex:
-    pass
+    print("Error:", sys.exc_info()[0])
+    print(sys.exc_info()[1])    
 finally:
     if capture is not None:
         capture.release()
