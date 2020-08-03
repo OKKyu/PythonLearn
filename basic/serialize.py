b5ed730c2b98a201ee127d92490a25d7e8078389
Kiwamu Nakasone <kyukyuinskt@gmail.com>
2020-08-03 09:40:06 +0900

    複数オブジェクトを同時保存する例として修正した

--- basic/serialize.py
+++ basic/serialize.py
@@ -4,18 +4,22 @@
 #http://dragstar.hatenablog.com/entry/2017/12/26/153214
 
 import pickle
-target = [1, 2, 3, 4, 5]
-savePath = '/home/puppy/pic/'
+target1 = [1, 2, 3, 4, 5]
+target2 = ['a', 'b', 'c', 'd', 'e']
+savePath = './'
 fname = 'test.pickle'
 
-print(target)
+print([x for x in zip(target1, target2)])
 
 # リストオブジェクトをpickleを使ってダンプ
 with open(savePath + fname, 'wb') as f:
-  pickle.dump(target, f)
+  pickle.dump(target1, f)
+  pickle.dump(target2, f)
 
 # pickleを使ってリストオブジェクトをロード
 with open(savePath + fname, 'rb') as f:
-  target2 = pickle.load(f)
+  target1_load = pickle.load(f)
+  target2_load = pickle.load(f)
 
-print(target2)
+print(target1_load)
+print(target2_load)
