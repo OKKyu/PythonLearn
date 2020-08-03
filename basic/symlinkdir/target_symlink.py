51d744f65d2a717350749d69a824107f6cf8cbc9
Kiwamu Nakasone <kyukyuinskt@gmail.com>
2020-08-03 09:34:35 +0900

    シンボルリンクでスクリプトを起動した場合の動作確認コードを追加

---
+++ basic/symlinkdir/target_symlink.py
@@ -0,0 +1,12 @@
+#!python3
+'''
+  testing run script throught symbolic link.
+  symbolic link has locate in parent dir.
+  And run the symlink with python command on console.
+  And Checking whick dir inputed into sys.path.
+'''
+import sys
+
+print("Hello! Symlinks")
+print(sys.path)
+
