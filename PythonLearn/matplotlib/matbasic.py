0eb1f2c84845993f2be10466f87a07b47846695f
OKKyu <kyukyuinskt@gmail.com>
2020-09-06 11:18:59 +0900

    matのサンプルコードを修正

--- PythonLearn/matplotlib/matbasic.py
+++ PythonLearn/matplotlib/matbasic.py
@@ -14,15 +14,23 @@ ax.plot([1, 2, 3], [1, 2, 3])
 plt.show()
 plt.style.use('ggplot')
 
-#グラフのタイトル
+#図を２個生成
 fig, axes = plt.subplots(2)
-axes[0].set_title('title one')
+#グラフのタイトル fontdictにスタイルを設定可能。まるでcss like.
+fontdic = {
+    'family':'serif',
+    'size':20,
+    'weight':'bold'
+}
+axes[0].set_title('title one', fontdict=fontdic)
 #軸ラベル
 axes[0].set_xlabel('horizon')
 axes[0].set_ylabel('vertical')
 #凡例
 axes[0].plot([1, 2, 3], [2, 4, 12], label='legend label')
 axes[0].legend(loc='best')
+#テキストを直接描画 x座標、y座標、テキスト、サイズ
+axes[1].text(0.2, 0.4, 'Text Direct Insert', size=14)
 plt.show()
 
 #保存も可能。eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, tiffがサポートされている。
