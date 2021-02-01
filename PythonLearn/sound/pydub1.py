566284b7af622a9478865ada4f7f648d6969fac2
OKKyu <kyukyuinskt@gmail.com>
2021-02-01 18:28:57 +0900

    ファイルをコマンドライン引数から入力する

--- PythonLearn/sound/pydub1.py
+++ PythonLearn/sound/pydub1.py
@@ -1,10 +1,17 @@
 #!python3
 # -*- coding:utf-8 -*-
+import sys
+import os
 from pydub import AudioSegment
 from pydub.playback import play
 
+# シェル引数からファイル名を受け取る
+if len(sys.argv) != 2 or os.path.exists(sys.argv[1]) == False:
+    print("Please input target audio file(mp3)")
+    sys.exit(1)
+
 # 音声ファイルの読み込み
-sound = AudioSegment.from_file("input.mp3", "mp3")
+sound = AudioSegment.from_file(sys.argv[1], "mp3")
 
 # 再生
 play(sound)
