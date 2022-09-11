645f25dc80b753881e740c3d53a8f9ac7955a648
OKKyu <kyukyuinskt@gmail.com>
2022-09-11 15:18:23 +0000

    Add new sample code
    smtp

--- PythonLearn/libsample/sample_smtplib.py
+++ PythonLearn/libsample/sample_smtplib.py
@@ -5,27 +5,25 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-#open smtp. 
-obj = smtplib.SMTP('smtp.gmail.com',587)
+# open smtp.
+obj = smtplib.SMTP('smtp.gmail.com', 587)
 
 obj.ehlo()
 obj.starttls()
 
-obj.login('kyukyuinskt@gmail.com','oapmvbiaxxddvrtn')
+obj.login('', '')
 
 
 msg = u'以下URLが本家サイト\nhttps://sqlitebrowser.org/dl/\nDebianだとaptで入れられるとのこと。\nsudo apt-get install sqlitebrowser\n'
 msg = msg + u'ちなみにこのメールはPythonで送信しているのだ！（特に意味はない）\n'
-info = MIMEText(msg,'plain','utf-8')
-info['Subject'] = Header(u'SQLiteツールあった','utf-8')
-info['From'] = 'kyukyuinskt@gmail.com'
-info['To']   = 'kyukyuinskt@gmail.com'
-info['To'] = 'tsuku.29@gmail.com'
+info = MIMEText(msg, 'plain', 'utf-8')
+info['Subject'] = Header(u'SQLiteツールあった', 'utf-8')
+info['From'] = ''
+info['To'] = ''
+info['To'] = ''
 
-#args can use string instead of MIMEText type.
+# args can use string instead of MIMEText type.
 obj.sendmail(info["From"], info["To"], info.as_string())
 
 obj.quit()
 
-#gmail app passwd
-#oapmvbiaxxddvrtn
